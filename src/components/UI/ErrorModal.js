3fabb594993f15d36edd2c736edeb2e25f2791cf
Astar <bahouidi99@gmail.com>
2023-08-20 22:55:13 +0100

    Portal for semantic html

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Card from './Card';
-import Button from './Button';
-import classes from './ErrorModal.module.css';
+import Card from "./Card";
+import Button from "./Button";
+import classes from "./ErrorModal.module.css";
 
-const ErrorModal = (props) => {
+function ModalBackdrop(props) {
+  return <div className={classes.backdrop} onClick={props.onConfirm} />;
+}
+
+function ModalDialog(props) {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
   );
-};
+}
 
-export default ErrorModal;
+export default function ErrorModal(props) {
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <ModalBackdrop onConfirm={props.onConfirm} />,
+        document.getElementById("modal-backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalDialog
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm} />,
+        document.getElementById("modal-dialog-root")
+      )}
+    </React.Fragment>
+  );
+}
